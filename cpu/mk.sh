b8ccf5c43f628a99237d3b9ac998126370740e6c
anferneeHu <anferneextt@126.com>
2016-12-27 14:35:55 +0800

    base cal

---
+++ cpu/mk.sh
@@ -0,0 +1,2 @@
+gcc t1.c -o t1;
+time t1 $1 $2;
---
+++ cpu/t1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double LARGENUM = 12345.6789;
+double OFFSET = 1234.567;
+
+
+
+
+void cal_per(int tms)
+{
+    double c = 0.0;
+    for (int i = 0; i < tms; i++)
+    {
+        c += LARGENUM * i - i * OFFSET;      
+    }
+    return ;
+}
+
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 3)
+    {
+        printf("usage t1 count tms\n");
+        return -1;
+    }
+
+    int count = atoi(argv[1]);
+    int tms = atoi(argv[2]);
+
+    printf("count is %d, tms is %d\n",count, tms);
+
+
+    for (int i = 0; i < count; i++ )
+    {
+        cal_per(tms);
+    }
+    return 0;
+}
+
